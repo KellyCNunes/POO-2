54d9c657e1e8b3556ca15e6b90feef7ecd418bb3
KellyCNunes <138325492+KellyCNunes@users.noreply.github.com>
2024-04-29 21:51:53 -0300

    atualizacao

--- PolimorfismoFuncionario/Program.cs
+++ PolimorfismoFuncionario/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PolimorfismoFuncionario;
+
+Funcionario f = new Funcionario(1, "Funcionario", 1000);
+System.Console.WriteLine("Bonificação do funcionario: " + f.CalcularBonificacao());
+
+Secretario s = new Secretario(2, "secretario", 1000);
+System.Console.WriteLine("Bonificação do secretario: " + s.CalcularBonificacao());
+
+Gerente g = new Gerente(3, "gerente", 1000);
+System.Console.WriteLine("Bonificação do gerente: " + g.CalcularBonificacao());
+
+Diretor d = new Diretor(4, "diretor", 1000);
+System.Console.WriteLine("Bonificação do diretor: " + d.CalcularBonificacao());
---
+++ heranca1/Perecivel.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace heranca1
+{
+    public class Perecivel : Produto
+    {
+        private string dataFabricacao;
+        private string dataValidade;
+        private int lote;
+
+        public Perecivel() : base()
+        {         }
+
+        public Perecivel (int codigo, string nome, double preco, string dataFabricacao, string dataValidade, int lote) : base (codigo, nome, preco)
+        {
+            DataFabricacao = dataFabricacao;
+            DataValidade = dataValidade;
+            Lote = lote;
+        }
+
+
+        //classe base= super classe= classe pai = classe mae
+        //classe filho = classe derivada = subclasse
+        public override void Mostrar()
+        {
+            base.Mostrar(); //base()-- chama construtor da classe base, agora base. ---- chama método da classe base (base.Metodo();)
+            Console.WriteLine("Data de Fabricação: " + DataFabricacao + "\tData Validade: " +  DataValidade + "\tLote: " + Lote);
+        }
+
+        public string DataFabricacao
+        {
+            get { return dataFabricacao; }
+            set { dataFabricacao = value; }
+        }
+
+        public string DataValidade
+        {
+            get { return dataValidade; }
+            set { dataValidade = value; }
+        }
+        
+        public int Lote
+        {
+            get { return lote; }
+            set { lote = value; }
+        }
+        
+    
+    }
+}
