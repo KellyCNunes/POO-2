70aae1e846e77b782254f3dc1f8a1d319c7135cf
KellyCNunes <138325492+KellyCNunes@users.noreply.github.com>
2024-05-20 19:51:20 -0300

    Add files via upload

---
+++ AbstratoCliente/Program.cs
@@ -0,0 +1,15 @@
+﻿// Método Main()
+
+using AbstratoCliente;
+
+//Cliente c = new Cliente();//não instanciará
+
+ClienteFisico cf = new ClienteFisico(1,"Ana","Rua Ana",20,1111);
+cf.Mostrar();
+
+ClienteJuridico cj = new ClienteJuridico(100, "Empresa", "Rua Empresa", 43,10000);
+cj.Mostrar();
+
+Teste t = new Teste();
+t.VerificaIdade(cj);
+
