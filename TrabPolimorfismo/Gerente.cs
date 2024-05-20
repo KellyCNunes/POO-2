805c6d7538c1e524c50b89bea49c29e34cbcb554
KellyCNunes <138325492+KellyCNunes@users.noreply.github.com>
2024-05-20 19:27:54 -0300

    Update Gerente.cs

--- TrabPolimorfismo/Gerente.cs
+++ TrabPolimorfismo/Gerente.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TrabalhoPolimorfismo
+namespace TrabPolimorfismo
 {
     public class Gerente : Funcionario
     {
